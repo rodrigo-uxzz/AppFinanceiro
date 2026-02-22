b92fa0b06b7ed015f336d8db32582f47a66e395a
rodrigo-uxzz <rodrigonito3@gmail.com>
2026-02-22 19:04:40 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import Splash from "./src/pages/Splash/index";
+import Home from "./src/pages/Home/index";
+import Login from "./src/pages/Login/index";
+import Cadastro from "./src/pages/Cadastro/index";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return(
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Splash' component={Splash}
+        options={{
+          headerShown: false
+
+        }}
+        />
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='Login' component={Login}
+        options={{
+          headerShown: false
+        }}
+        />
+        <Stack.Screen name='Cadastro' component={Cadastro}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
