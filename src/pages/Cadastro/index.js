a706021123ef628fb486f7cf863a2570640a0b0e
Roberto Felix <90075190+Roberto180@users.noreply.github.com>
2026-02-23 00:45:02 -0300

    Inter banco

--- src/pages/Cadastro/index.js
+++ src/pages/Cadastro/index.js
@@ -1,23 +1,55 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View, TextInput } from 'react-native';
+import { Text, View, TextInput, Image, Pressable } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 import styles from "./style";
 
 export default function App() {
   return (
     <View style={styles.container}>
         <View style={styles.v1}>
-            
+            <Image 
+            source={require("../../../assets/Logo_do_banco_Inter_(2023).svg.png")}
+            style={styles.logoSplash}
+            />
+        </View>
+
+        <View style={styles.v2}>
+
+            <Text style={styles.textoA}>Boas vindas ao Inter e à uma vida financeira mais inteligente!</Text>
             <TextInput
-                style={styles.user}
-                placeholder="User"
+                style={styles.cadastro}
+                placeholder="Nome completo"
+                />
+
+            <TextInput
+                style={styles.cadastro}
+                placeholder='CPF'
+                />
+
+            <TextInput
+                style={styles.cadastro}
+                placeholder='e-mail'
             />
 
             <TextInput
-                style={styles.senha}
-                placeholder='Password'
-            />
+                style={styles.cadastro}
+                placeholder='celular'
+            />   
 
         </View>
+        <View style={styles.v3}>
+
+            <Pressable 
+              style={({pressed}) => [
+                styles.batom1,
+                pressed && styles.batomPress1
+              ]}
+              >
+                <Text style={styles.texto1}>Entrar</Text> 
+            </Pressable>
+
+        </View>
+
       <StatusBar style="auto" />
     </View>
   );
--- src/pages/Login/style.js
+++ src/pages/Login/style.js
@@ -2,10 +2,122 @@ import { StyleSheet } from "react-native";
 
 
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+container:{
+    flex:1,
+    backgroundColor: "#1a1d22",
+},
+v1:{
+    flex:0.20,
+    justifyContent: "flex-start",
+    alignItems: "flex-end",
+},
+
+v2:{
+    flex:1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 200,
+},
+
+//imagens inter
+iconsTop:{
+    width: 120, 
+    height: 35,
+    resizeMode: "contain",
+    marginTop: 5,
+},
+
+logoSplash:{
+    width: 150, 
+    height: 100,
+    resizeMode: "contain",
+    marginBottom: 15,
+},
+
+//parte do login
+
+login:{
+    backgroundColor: "#1a1d22",
+    width: 330,
+    height: 50,
+    borderRadius: 15,
+    marginBottom: 10,
+    padding: 10,
+    borderColor: "#fff",
+    borderWidth: 1.6,
+    color: "#fff",
+    
+},
+
+// botoes de entrar e esqueci
+
+v2:{
+    flex:0.50,
+    justifyContent: "center",
+    alignItems: "center",
+},
+
+
+batom1:{
+    backgroundColor: "#FF7A00",
+    width: 330,
+    height: 50,
+    padding: 15,
+    borderRadius: 50,
+    textAlign: "center",
+    color: "#fff",
+    marginTop: 20,
+    marginBottom: 8,
+},
+
+//efeito de clique 1
+  batomPress1: {
+    opacity: 0.9,
+    transform: [{ scale: 0.98 }], 
   },
+     texto1: {
+      color: '#FFF',
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+
+
+batom2:{
+    backgroundColor: "#FF7A0030",
+    width: 330,
+    height: 50,
+    padding: 15,
+    borderRadius: 50,
+    color: "#FF7A00",
+    marginBottom: 5,   
+},
+//efeito de clique 2
+  batomPress2: {
+    backgroundColor: '#fc973890', 
+    opacity: 0.9,
+    transform: [{ scale: 0.98 }], 
+  },
+    texto2: {
+     color: '#FF7A00',
+     fontWeight: 'bold',
+     textAlign: 'center',
+    },
+
+texto3:{
+    color: '#FF7A00',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    marginTop: 5,
+},
+
+
+//efeito de clique 3 no cadastre-se
+  textPress3: {
+    color: '#fc973890',
+    textDecorationLine: 'underline',
+    opacity: 0.9,
+    transform: [{ scale: 0.98 }], 
+  },
+
+
 });
